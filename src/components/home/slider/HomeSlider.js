0c5a3bce18524bbeb62de052f271fcf7580bf47e
Leandro-Andres-Angeli <leandroandresangeli@gmail.com>
2023-01-14 19:16:30 -0300

    refactoring slider , refactored slider text and img

--- src/components/home/slider/HomeSlider.js
+++ src/components/home/slider/HomeSlider.js
@@ -1,7 +1,8 @@
 import AnimatedButton from '../../shared/AnimatedButton';
 import './home_slider.scss';
+import { SliderImg, SliderTextContent } from './SliderCompents';
+
 const HomeSlider = (randomMeals) => {
-  // <button class=' btn btn-primary carousel-btn'>order now</button>;
   const btnSliderArticle = new AnimatedButton('order now')
     .addClassLists(['carousel-btn'])
     .build();
@@ -12,60 +13,112 @@ const HomeSlider = (randomMeals) => {
   );
 
   // data-bs-ride="carousel"
-  return (
-    randomMeals
-      .map(({ meals }, i) => {
-        const { strMeal, strSource, strMealThumb, strInstructions } = meals[0];
-        console.log(strMeal);
-        return (
-          (i === 0 &&
-            `<div id="carouselExampleControls"   class="carousel slide  py-5" >
-            
-<div class="carousel-inner  p-4">
-<div class="carousel-item active ">
-  
+  let carousel = `<div id="carouselExampleControls"   class="carousel slide  py-5" >
+  <div class="carousel-inner  p-4">`;
+  randomMeals.map(({ meals }, i) => {
+    // <div class='col-12 col-md-6 p-2 p-md-4  d-flex flex-column gap-4 '>
+    // <h6>Welcome to out store</h6>
+    // <h2 class='text-primary'>${strMeal}</h2>
+    // <p>${strInstructions}</p>
+    // ${btnSliderArticle} </div>
+    const { strMeal, strSource, strMealThumb, strInstructions } = meals[0];
+    // console.log(strMeal);
+    const sliderTextContent = new SliderTextContent(
+      strMeal,
+      strInstructions,
+      undefined
+    );
+    const sliderImg = new SliderImg(strMealThumb);
+    return (carousel += `<div class="carousel-item ${
+      (i === 0 && 'active') || ''
+    }  ">
+
   <div class='row'>
-  <div class='col-12 col-md-6 p-2 p-md-4  d-flex flex-column gap-4 '>
-  <h6>Welcome to out store</h6>
-  <h2 class='text-primary'>${strMeal}</h2>
-  <p>${strInstructions}</p>
-  ${btnSliderArticle}
-            
-  </div>
-  <div class='col-6 d-none d-md-block'   >
-            <img src="${strMealThumb}" class="img-fluid carousel-img" alt="...">
-  </div>
-  </div>
+    ${sliderTextContent.build()}
+    ${sliderImg.build()}
+</div></div>
+
   
-
-</div>`) ||
-          ` <div class="carousel-item ">
-
-<div class='row'>
-<div class='col-12 col-md-6 p-2 p-md-4  d-flex flex-column gap-4 '>
-<h6>Welcome to out store</h6>
-<h2 class='text-blue'>${strMeal}</h2>
-<p>${strInstructions}</p>
-${btnSliderArticle}
-</div>
-<div class='col-6 d-none d-md-block'   >
-            <img src="${strMealThumb}" class="img-fluid carousel-img" alt="...">
-  </div>
-</div>
-</div>`
-        );
-      })
-      .join('') +
-    '</div>' +
-    `<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-  <span class="visually-hidden">Previous</span>
-</button>
-<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-  <span class="visually-hidden">Next</span>
-</button>
-</div>`
-  );
+ `);
+  });
+  carousel += `</div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+   <span class="visually-hidden">Previous</span>
+ </button>
+ <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+   <span class="carousel-control-next-icon" aria-hidden="true"></span>
+   <span class="visually-hidden">Next</span>
+ </button>
+  </div>`;
+  return carousel;
 };
+
+// const HomeSlider = (randomMeals) => {
+
+//   const btnSliderArticle = new AnimatedButton('order now')
+//     .addClassLists(['carousel-btn'])
+//     .build();
+//   console.log(btnSliderArticle);
+//   document.documentElement.style.setProperty(
+//     '--navbar-height',
+//     document.querySelector('nav').getClientRects()[0].height + 'px'
+//   );
+
+//   // data-bs-ride="carousel"
+//   return (
+//     randomMeals
+//       .map(({ meals }, i) => {
+//         const { strMeal, strSource, strMealThumb, strInstructions } = meals[0];
+//         console.log(strMeal);
+//         return (
+//           (i === 0 &&
+//             `<div id="carouselExampleControls"   class="carousel slide  py-5" >
+
+// <div class="carousel-inner  p-4">
+// <div class="carousel-item active ">
+
+//   <div class='row'>
+//   <div class='col-12 col-md-6 p-2 p-md-4  d-flex flex-column gap-4 '>
+//   <h6>Welcome to out store</h6>
+//   <h2 class='text-primary'>${strMeal}</h2>
+//   <p>${strInstructions}</p>
+//   ${btnSliderArticle}
+
+//   </div>
+//   <div class='col-6 d-none d-md-block'   >
+//             <img src="${strMealThumb}" class="img-fluid carousel-img" alt="...">
+//   </div>
+//   </div>
+
+// </div>`) ||
+//           ` <div class="carousel-item ">
+
+// <div class='row'>
+// <div class='col-12 col-md-6 p-2 p-md-4  d-flex flex-column gap-4 '>
+// <h6>Welcome to out store</h6>
+// <h2 class='text-blue'>${strMeal}</h2>
+// <p>${strInstructions}</p>
+// ${btnSliderArticle}
+// </div>
+// <div class='col-6 d-none d-md-block'   >
+//             <img src="${strMealThumb}" class="img-fluid carousel-img" alt="...">
+//   </div>
+// </div>
+// </div>`
+//         );
+//       })
+//       .join('') +
+//     '</div>' +
+//     `<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+//   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+//   <span class="visually-hidden">Previous</span>
+// </button>
+// <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+//   <span class="carousel-control-next-icon" aria-hidden="true"></span>
+//   <span class="visually-hidden">Next</span>
+// </button>
+// </div>`
+//   );
+// };
 export default HomeSlider;
---
+++ src/components/home/slider/SliderCompents.js
@@ -0,0 +1,41 @@
+import AnimatedButton from '../../shared/AnimatedButton';
+
+const btnSliderArticle = new AnimatedButton('order now')
+  .addClassLists(['carousel-btn'])
+  .build();
+class SliderComponent {
+  constructor() {}
+  build() {
+    return this.tag;
+  }
+}
+class SliderTextContent extends SliderComponent {
+  constructor(title, textContext, btn = btnSliderArticle) {
+    super();
+    this.title = title;
+    this.textContext = textContext;
+
+    this.btn = btn;
+    this.tag = ` 
+    <div class='col-12 col-md-6 p-2 p-md-4  d-flex flex-column gap-4 '>
+    <h6>Welcome to out store</h6>
+        <h2 class='text-primary'>${title}</h2>
+        <p>${textContext}</p>
+        ${btn}
+                  
+        </div>
+      `;
+  }
+}
+class SliderImg extends SliderComponent {
+  constructor(src) {
+    super();
+    this.tag = ` 
+      <div class='col-6 d-none d-md-block'   >
+            <img src="${src}" class="img-fluid carousel-img" alt="...">
+  </div>
+      `;
+  }
+}
+
+export { SliderTextContent, SliderImg };
