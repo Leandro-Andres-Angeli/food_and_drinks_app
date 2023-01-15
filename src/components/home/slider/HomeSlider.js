e047fbe8d0592cbb5f275ae3789ddbe105fbfeac
Leandro-Andres-Angeli <leandroandresangeli@gmail.com>
2023-01-15 19:41:21 -0300

    animated buttons slider functionality added

--- src/components/home/slider/HomeSlider.js
+++ src/components/home/slider/HomeSlider.js
@@ -12,7 +12,9 @@ const HomeSlider = (randomMeals) => {
     document.querySelector('nav').getClientRects()[0].height + 'px'
   );
 
-  let carousel = `<div id="carouselExampleControls"   class="carousel slide  py-5 px-md-5" >
+  let carousel = `
+  <main>
+  <div id="carouselExampleControls"   class="carousel slide  py-5 px-md-5" >
   <div class="carousel-inner  p-4">`;
   randomMeals.map(({ meals }, i) => {
     const { strMeal, strSource, strMealThumb, strInstructions } = meals[0];
@@ -42,84 +44,9 @@ const HomeSlider = (randomMeals) => {
   ].map((btnData) => {
     return (carousel += new SliderBtn(btnData).build());
   });
-  carousel += `</div>`;
-  //   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-  //    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-  //    <span class="visually-hidden">Previous</span>
-  //  </button>
-  //  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-  //    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-  //    <span class="visually-hidden">Next</span>
-  //  </button>
-  //   </div>`;
+  carousel += `</div></main>`;
+
   return carousel;
 };
 
-// const HomeSlider = (randomMeals) => {
-
-//   const btnSliderArticle = new AnimatedButton('order now')
-//     .addClassLists(['carousel-btn'])
-//     .build();
-//   console.log(btnSliderArticle);
-//   document.documentElement.style.setProperty(
-//     '--navbar-height',
-//     document.querySelector('nav').getClientRects()[0].height + 'px'
-//   );
-
-//   // data-bs-ride="carousel"
-//   return (
-//     randomMeals
-//       .map(({ meals }, i) => {
-//         const { strMeal, strSource, strMealThumb, strInstructions } = meals[0];
-//         console.log(strMeal);
-//         return (
-//           (i === 0 &&
-//             `<div id="carouselExampleControls"   class="carousel slide  py-5" >
-
-// <div class="carousel-inner  p-4">
-// <div class="carousel-item active ">
-
-//   <div class='row'>
-//   <div class='col-12 col-md-6 p-2 p-md-4  d-flex flex-column gap-4 '>
-//   <h6>Welcome to out store</h6>
-//   <h2 class='text-primary'>${strMeal}</h2>
-//   <p>${strInstructions}</p>
-//   ${btnSliderArticle}
-
-//   </div>
-//   <div class='col-6 d-none d-md-block'   >
-//             <img src="${strMealThumb}" class="img-fluid carousel-img" alt="...">
-//   </div>
-//   </div>
-
-// </div>`) ||
-//           ` <div class="carousel-item ">
-
-// <div class='row'>
-// <div class='col-12 col-md-6 p-2 p-md-4  d-flex flex-column gap-4 '>
-// <h6>Welcome to out store</h6>
-// <h2 class='text-blue'>${strMeal}</h2>
-// <p>${strInstructions}</p>
-// ${btnSliderArticle}
-// </div>
-// <div class='col-6 d-none d-md-block'   >
-//             <img src="${strMealThumb}" class="img-fluid carousel-img" alt="...">
-//   </div>
-// </div>
-// </div>`
-//         );
-//       })
-//       .join('') +
-//     '</div>' +
-//     `<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-//   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-//   <span class="visually-hidden">Previous</span>
-// </button>
-// <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-//   <span class="carousel-control-next-icon" aria-hidden="true"></span>
-//   <span class="visually-hidden">Next</span>
-// </button>
-// </div>`
-//   );
-// };
 export default HomeSlider;
--- src/views/Home.js
+++ src/views/Home.js
@@ -29,6 +29,40 @@ export const Home = async () => {
     randomMeal(),
     randomMeal(),
   ]);
+  document.addEventListener('mouseover', function (e) {
+    if (
+      !e.target.closest('main') ||
+      !e.target
+        .closest('main')
+        .querySelector('div')
+        .classList.contains('carousel')
+    )
+      return;
 
+    const sliderBtns = [
+      ...document.querySelectorAll('#carouselExampleControls .slider_btn'),
+    ].map((btnContainer) => btnContainer.querySelector('span'));
+    sliderBtns.forEach((sliderBtn) => {
+      return sliderBtn.classList.add('show_btn');
+    });
+  });
+  document.addEventListener('mouseout', function (e) {
+    if (
+      e.target.closest('main') !== null &&
+      e.target
+        .closest('main')
+        .querySelector('div')
+        .classList.contains('carousel')
+    ) {
+      const sliderBtns = [
+        ...document.querySelectorAll('#carouselExampleControls .slider_btn'),
+      ].map((btnContainer) => btnContainer.querySelector('span'));
+      sliderBtns.forEach((sliderBtn) => {
+        return sliderBtn.classList.remove('show_btn');
+      });
+    } else {
+      return;
+    }
+  });
   return HomeSlider(randomMeals);
 };
