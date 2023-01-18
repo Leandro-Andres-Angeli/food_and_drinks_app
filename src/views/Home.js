8c2f268d6c12075dc68365c9766c9a7744ecd1c9
Leandro-Andres-Angeli <leandroandresangeli@gmail.com>
2023-01-18 00:49:58 -0300

    refactored animated buttons function and tree shaking

--- src/views/Home.js
+++ src/views/Home.js
@@ -21,14 +21,6 @@ export const Home = async () => {
       },
       condition: function (ev) {
         return Boolean(!!ev.closest?.('main'));
-        // ev.closest !== null ||
-        // ev.closest('main').querySelector('div').classList.contains('carousel')
-
-        //||
-        //   ev.target
-        //     .closest('main')
-        //     .querySelector('div')
-        //     .classList.contains('carousel');
       },
     },
     {
@@ -42,19 +34,7 @@ export const Home = async () => {
       condition: function (ev) {
         console.log(ev);
         return Boolean(ev.closest?.('main'));
-
-        // !== null &&
-        //   ev.target
-        //     .closest('main')
-        //     .querySelector('div')
-        //     .classList.contains('carousel');
       },
-      // condition:
-      //   e.closest('main') !== null &&
-      //   e
-      //     .closest('main')
-      //     .querySelector('div')
-      //     .classList.contains('carousel'),
     },
   ].map(({ mouseEvent, log, condition, action }) => {
     document.addEventListener(mouseEvent, function (e) {
@@ -71,67 +51,7 @@ export const Home = async () => {
             return sliderBtn.classList[`${action}`]('show_btn');
           })
         : null;
-      // if (mEvent.condition(e)) {
-      //   const sliderBtns = [
-      //     ...document.querySelectorAll('#carouselExampleControls .slider_btn'),
-      //   ].map((btnContainer) => btnContainer.querySelector('span'));
-      //   sliderBtns.forEach((sliderBtn) => {
-      //     return sliderBtn.classList.toggle('show_btn');
-      //   });
-      // } else return;
     });
   });
-  // document.addEventListener('mouseover', function (e) {
-  //   if (
-  //     e.target.closest('main') ||
-  //     e.target
-  //       .closest('main')
-  //       .querySelector('div')
-  //       .classList.contains('carousel')
-  //   ) {
-  //     const sliderBtns = [
-  //       ...document.querySelectorAll('#carouselExampleControls .slider_btn'),
-  //     ].map((btnContainer) => btnContainer.querySelector('span'));
-  //     sliderBtns.forEach((sliderBtn) => {
-  //       return sliderBtn.classList.add('show_btn');
-  //     });
-  //   } else return;
-  // });
-
-  ///////////////
-  // if (
-  //   !e.target.closest('main') ||
-  //   !e.target
-  //     .closest('main')
-  //     .querySelector('div')
-  //     .classList.contains('carousel')
-  // )
-  //   return;
-
-  // const sliderBtns = [
-  //   ...document.querySelectorAll('#carouselExampleControls .slider_btn'),
-  // ].map((btnContainer) => btnContainer.querySelector('span'));
-  // sliderBtns.forEach((sliderBtn) => {
-  //   return sliderBtn.classList.add('show_btn');
-  // });
-  //////////
-  // document.addEventListener('mouseout', function (e) {
-  //   if (
-  //     e.target.closest('main') !== null &&
-  //     e.target
-  //       .closest('main')
-  //       .querySelector('div')
-  //       .classList.contains('carousel')
-  //   ) {
-  //     const sliderBtns = [
-  //       ...document.querySelectorAll('#carouselExampleControls .slider_btn'),
-  //     ].map((btnContainer) => btnContainer.querySelector('span'));
-  //     sliderBtns.forEach((sliderBtn) => {
-  //       return sliderBtn.classList.remove('show_btn');
-  //     });
-  //   } else {
-  //     return;
-  //   }
-  // });
   return HomeSlider(randomMeals);
 };
