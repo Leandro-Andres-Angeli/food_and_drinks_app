9e4041359a797ab151d042eb92d7aaaefc4c06fb
Leandro-Andres-Angeli <leandroandresangeli@gmail.com>
2023-01-22 17:30:14 -0300

    picture srcset done

--- src/views/about/AboutComponents.js
+++ src/views/about/AboutComponents.js
@@ -1,16 +1,44 @@
-import * as image from '../../../static/assets/img/spaghetti.png';
+import * as image from '../../../static/assets/img/spaghetti.jpg';
+import * as imageMobile from '../../../static/assets/img/spaghetti-xs.jpg';
+import * as imageTablet from '../../../static/assets/img/spaghetti-sm.jpg';
 import PageSection, { mainHeader } from '../../components/shared/PageSection';
+import './about_styles.scss';
 export const mainSection = new PageSection('main').setContent(
   mainHeader('About', 'about us')
 );
 const aboutInfo = new PageSection('section');
 
+/* <img class='img-fluid rounded shadow-lg ${
+      window.innerWidth >= 768 ? 'w-100' : 'w-75'
+    }' lazy='true' src=${image}/> */
+
+{
+  /* <picture class='img-fluid'>
+  <source media="(min-width:1201px)" srcset="${image}" type="image/png">
+  <source media="(max-width:1200px)" srcset="${imageMobile}" type="image/png">
+  <img  class='img-fluid' src="${image}"/>
+ 
+</picture>  */
+}
+
 aboutInfo.setContent(
   `<div class='row p-5 text-center gap-5 gap-lg-0'>
-  <div class='col col-12 col-lg-6 '>
-    <img class='img-fluid rounded shadow-lg ${
-      window.innerWidth >= 768 ? 'w-100' : 'w-75'
-    }' lazy='true' src=${image}/>
+  <div class='col col-12 col-lg-6 img-about-info-container  m-auto'>
+<picture>
+
+       <source media="(min-width: 1200px)"
+                srcset=
+"${image}">
+ <source media="(min-width: 800px)"
+                srcset=
+"${imageTablet}">
+ 
+<img src=
+"${imageMobile}"
+class='img-fluid rounded shadow-lg'
+alt="" >
+</picture> 
+    
   </div>
   <div class='col col-12 col-lg-6 my-lg-5 my-0 py-5 py-lg-0'>
   <h2 class='text-primary fw-bold'>
