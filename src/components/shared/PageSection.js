e48dd464dc62f3d8398364095d82004fa610e598
Leandro-Andres-Angeli <leandroandresangeli@gmail.com>
2023-01-21 21:40:48 -0300

    serving static img

--- src/components/shared/PageSection.js
+++ src/components/shared/PageSection.js
@@ -18,18 +18,18 @@ export const mainHeader = (
   subtitle = 'contact us',
   classes = ['background-yellow-100']
 ) => {
-  return (sectionText = `<header class='mt-4 p-5 text-center ${classes
+  return `<header class='mt-4 p-5 text-center ${classes
     .map((cl) => cl)
     .join(' ')}  '>
     <h1 >${title}</h1>
     <p>${subtitle}</p>
-  </header>`);
+  </header>`;
 };
 export const innerCard = (title, classLists) => {
-  return (sectionText = `<div class="mt-4 p-5 text-center background-yellow-100  ">
+  return `<div class="mt-4 p-5 text-center background-yellow-100  ">
     <h1 >${title}</h1>
     <p>${subtitle}</p>
-  </div>`);
+  </div>`;
 };
 
 export default PageSection;
