3a83f3dc200b9593e3c30286aaa6b58bb05ee6c8
Leandro-Andres-Angeli <leandroandresangeli@gmail.com>
2023-01-26 01:03:51 -0300

    refactoring footer elements

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,15 +1,15 @@
-import { arrowIcon } from '../../utils/icons';
+import { iconsList } from '../../utils/icons/icons';
 import './footer_styles.scss';
 
-console.log(arrowIcon);
-console.log(document.body.get);
 class FooterComponent {
   static tag;
   static classLists;
+
   static setTag(tagSetup) {
     this.tag = tagSetup;
     return this;
   }
+
   static setInnerTags(innerTags) {
     this.tag.innerHTML = innerTags;
     return this;
@@ -19,6 +19,12 @@ class FooterComponent {
     return this;
   }
 }
+
+const FooterList = FooterComponent;
+
+FooterList.icon = iconsList['arrowIcon'].component;
+const footerListEl = FooterList;
+footerListEl.setTag(footerListEl.icon);
 
 const listItems = [
   'Privacy Policy',
@@ -30,29 +36,67 @@ const listItems = [
 ]
   .reduce((acc, current) => {
     acc.push(
-      FooterComponent.setTag(
-        `<li class='list-group-item'>
-        <a href='#' class='list-group-item-anchor'> ${arrowIcon} ${current}</a>
-        </li>`
-      ).tag
+      footerListEl.setTag(`<li class='list-group-item ps-0'>
+        <a href='#' class='list-group-item-anchor '> ${
+          footerListEl.icon && footerListEl.icon
+        }  ${current}</a>
+        </li>`).tag
     );
     return acc;
   }, [])
   .join('');
-console.log(listItems);
+const itemsTextContent = [
+  'Privacy Policy',
+  'Terms & Conditions',
+  'Order Tracking',
+  'Refunds & Returns',
+  'Our Services',
+  'Career Opportunity ',
+];
+// footerListEl.setTag(`<li class='list-group-item ps-0'>
+// <a href='#' class='list-group-item-anchor '> ${
+//   footerListEl.icon && footerListEl.icon
+// }  ${current}</a>
+// </li>`).tag
+const generateList = (textContent, renderListFunc) => {
+  return textContent
+    .reduce((acc, current) => {
+      acc.push(renderListFunc(current));
+      return acc;
+    }, [])
+    .join('');
+};
+const generateUl = () => {};
+renderListTag = (textContent) => {
+  footerListEl.clean();
+  return footerListEl.setTag(`<li class='list-group-item ps-0'>
+  <a href='#' class='list-group-item-anchor '> ${
+    footerListEl.icon && footerListEl.icon
+  }  ${textContent}</a>
+ </li>`).tag;
+};
+const servicesList = generateList(itemsTextContent, renderListTag);
+
+const footerTitle = function (title) {
+  `<h2 class='footer-section-title fs-4 position-relative'>${title}</h2> `;
+};
 const listFooter = FooterComponent.setTag(
   (() => {
     const tag = document.createElement('ul');
+
     'list-group quick-link'.split(' ').forEach((cl) => tag.classList.add(cl));
+
     return tag;
   })()
-).setInnerTags(listItems).tag;
+).setInnerTags(footerTitle('Quick Link') + listItems).tag;
 const listCol = FooterComponent.setTag(
   (() => {
     const element = document.createElement('div');
+
     'col col-12 col-md-6 col-lg-3'
       .split(' ')
       .forEach((cl) => element.classList.add(cl));
+
     return element;
   })()
 ).setInnerTags(listFooter.outerHTML);
