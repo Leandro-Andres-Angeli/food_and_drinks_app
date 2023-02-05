97fcb6aa59b552fe3d97cd77dff2412ee31a3854
Leandro-Andres-Angeli <leandroandresangeli@gmail.com>
2023-02-05 18:42:33 -0300

    env variables added and category component in progress

--- src/views/category/CategoryComponent.js
+++ src/views/category/CategoryComponent.js
@@ -1,7 +1,12 @@
-const CategoryComponent = () => {
+import getData from '../../apis/getData';
+
+const CategoryComponent = async () => {
   const split = window.location.hash.split('?');
   const category = split.pop().replaceAll('category=', '').trim();
-  console.log(category);
+  const categoryFetch = await getData(
+    `${process.env.API_ENDPOINT}filter.php?c=${category}`
+  );
+  console.log(categoryFetch);
   return `<div> <h1>Category</h1></div>`;
 };
 export default CategoryComponent;
