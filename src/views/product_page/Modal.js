98a0ac3c12220dcb2f98f551c6f35aab151ead3a
Leandro_Angeli <leandroandresangeli@gmail.com>
2023-03-06 23:44:55 -0300

    fixing card styles

--- src/views/product_page/Modal.js
+++ src/views/product_page/Modal.js
@@ -3,7 +3,7 @@ import { iconsList } from "../../utils/icons/icons";
 
 
 export const modal = `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-<div class="modal-dialog modal-dialog-centered w-50"  style="max-width:100vw">
+<div class="modal-dialog modal-dialog-centered w-100 w-md-74" >
 <div class='loader-container'>${iconsList.loader.component}</div>
   <div class="modal-content">
     <div class="modal-header">
