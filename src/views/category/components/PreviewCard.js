644e86f3751b5a787fcc484a22125ac119526e70
Leandro_Angeli <leandroandresangeli@gmail.com>
2023-03-13 17:47:47 -0300

    fixed preview card overflow

--- src/views/category/components/PreviewCard.js
+++ src/views/category/components/PreviewCard.js
@@ -11,7 +11,7 @@ const previewCard = function (data , rating = 'rating-container') {
     const [name, img,id] = Object.values(arguments[0]);
    
     return `<li class="list-group-item d-flex border-0 preview-card " data-id="${id}">
-    <a href="${window.location.hash}&id=${id}" class='preview-product-card-link d-flex text-decoration-none'>     <img class=' img-fluid w-lg-50 w-25' src="${img}"/>
+    <a href="${window.location.hash}&id=${id}" class='preview-product-card-link overflow-hidden d-flex text-decoration-none'>     <img class=' img-fluid w-lg-50 w-25' src="${img}"/>
      <div class='text-truncate my-3 mx-1 ps-2'>
       <h2 class='fs-6 text-primary text-truncate mw-75 hover-secondary'>${name}</h2>
      ${ rating &&  `<${rating}></${rating}>`}
