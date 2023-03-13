b7e2f929d48af3a44b6b833da7dc8775496e4e63
Leandro_Angeli <leandroandresangeli@gmail.com>
2023-03-13 09:24:55 -0300

    added link to preview card

--- src/views/category/components/PreviewCard.js
+++ src/views/category/components/PreviewCard.js
@@ -1,18 +1,27 @@
 import RatingComponent from "./RatingComponent";
-const previewCard = function (data) {
-   
+const previewCard = function (data , rating = 'rating-container') {
+    this.addEventListener('click',(e)=>{
+      
+      if(e.target.closest('a') === null ||   !e.target.closest('a').classList.contains('preview-product-card-link')){
+         return;  
+      }else
+      window.location = e.target.closest('a').href;
+      
+    })
     const [name, img,id] = Object.values(arguments[0]);
    
     return `<li class="list-group-item d-flex border-0 preview-card " data-id="${id}">
-     <img class=' img-fluid w-lg-50 w-25' src="${img}"/>
+    <a href="${window.location.hash}&id=${id}" class='preview-product-card-link d-flex text-decoration-none'>     <img class=' img-fluid w-lg-50 w-25' src="${img}"/>
      <div class='text-truncate my-3 mx-1 ps-2'>
       <h2 class='fs-6 text-primary text-truncate mw-75 hover-secondary'>${name}</h2>
-      <rating-container></rating-container>
-     <p> ${new Intl.NumberFormat('es-AR', {
+     ${ rating &&  `<${rating}></${rating}>`}
+     <p class='text-gray-700'> ${new Intl.NumberFormat('es-AR', {
        style: 'currency',
        currency: 'ARS',
      }).format(25)}</p>
       </div>
+      </a>
+
     </li>`;
   };
 export default previewCard  
--- src/views/search_page/SearchPage.js
+++ src/views/search_page/SearchPage.js
@@ -1,5 +1,7 @@
+
 const searchView =  (searchResults)=>{
     console.log(searchResults)
+
    return `<div class="container" > ${JSON.stringify(searchResults)}</div> `
 }
 export default searchView;
