4f6141a293cc16324b399b90348e337c0eef2ef0
Leandro_Angeli <leandroandresangeli@gmail.com>
2023-03-14 21:49:25 -0300

    fixing modal

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -10,6 +10,11 @@ const Contact = () => {
     <div class='p-2 p-md-3 p-lg-3 background-yellow-100' >
     <form class='background-gray-100 p-4  w-75 m-auto' >
     <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Name</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+    <div class="mb-3">
       <label for="exampleInputEmail1" class="form-label">Email address</label>
       <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
       <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
@@ -18,10 +23,7 @@ const Contact = () => {
       <label for="exampleInputPassword1" class="form-label">Password</label>
       <input type="password" class="form-control" id="exampleInputPassword1">
     </div>
-    <div class="mb-3 form-check">
-      <input type="checkbox" class="form-check-input" id="exampleCheck1">
-      <label class="form-check-label" for="exampleCheck1">Check me out</label>
-    </div>
+   
     <button type="submit" class="btn btn-primary">Submit</button>
   </form>
   </div>
--- src/views/product_page/Modal.js
+++ src/views/product_page/Modal.js
@@ -5,7 +5,7 @@ import { iconsList } from "../../utils/icons/icons";
 export const modal = `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
 <div class="modal-dialog modal-dialog-centered w-100 w-md-74" >
 <div class='loader-container'>${iconsList.loader.component}</div>
-  <div class="modal-content p-3">
+  <div class="modal-content p-3" style="max-width:100vw">
     <div class="modal-header">
       <h5 class="modal-title">Modal title</h5>
       <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
